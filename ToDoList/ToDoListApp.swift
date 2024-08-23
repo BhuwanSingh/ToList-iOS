90d8cfa7fcd36c1640e2a0c228b5e54e79058b7f
Bhuwan Singh <66213701+BhuwanSingh@users.noreply.github.com>
2024-08-23 16:23:35 +0530

    initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Bhuwan Singh on 23/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
